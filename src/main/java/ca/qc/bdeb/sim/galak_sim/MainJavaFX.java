6f75a908161a5a96b902ed476895533f4d63c697
Vahan Israyelyan <65795537+vahanisrayelyan@users.noreply.github.com>
2026-02-09 16:59:59 -0500

    New config

--- src/main/java/ca/qc/bdeb/sim/galak_sim/MainJavaFX.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/MainJavaFX.java
@@ -2,13 +2,12 @@ package ca.qc.bdeb.sim.galak_sim;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainJavaFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane borderPane = new BorderPane();
