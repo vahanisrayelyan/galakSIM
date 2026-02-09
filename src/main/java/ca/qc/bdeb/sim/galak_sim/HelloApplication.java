793cdd88fdeff066fb1323163c78015b2e654525
MATITO3 <matlanglois2007@gmail.com>
2026-02-09 17:00:44 -0500

    Test Matis

--- src/main/java/ca/qc/bdeb/sim/galak_sim/HelloApplication.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/HelloApplication.java
@@ -12,8 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane borderPane = new BorderPane();
-        Scene scene = new Scene(borderPane, 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(borderPane, 1200, 800);
+        stage.setTitle("GalakSIM");
         stage.setScene(scene);
         stage.show();
     }
