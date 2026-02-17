8dba3aa72c1f84f285db5f922f7c2ed24941952b
MatisLanglois <matlanglois2007@gmail.com>
2026-02-16 20:37:34 -0500

    Animation timer

--- src/main/java/ca/qc/bdeb/sim/galak_sim/MainJavaFX.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/MainJavaFX.java
@@ -1,26 +1,46 @@
 package ca.qc.bdeb.sim.galak_sim;
 
+import ca.qc.bdeb.sim.galak_sim.graphics.Simulation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainJavaFX extends Application {
+    private static Simulation simulation;
+    public final static double LARGEUR = 1200;
+    public final static double HAUTEUR = 700;
+
     @Override
     public void start(Stage stage) throws IOException {
-        BorderPane borderPane = new BorderPane();
-        Scene scene = new Scene(borderPane, 1200, 700);
-        var imageExo = new Image("TrainExoRobuste.jpg");
-        var vueImageExo = new ImageView(imageExo);
-        vueImageExo.setX(450);
-        vueImageExo.setY(275);
-        borderPane.getChildren().add(vueImageExo);
+        simulation = new Simulation();
+        var canvas = new Canvas(LARGEUR, HAUTEUR);
+        var contexte = canvas.getGraphicsContext2D();
 
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane, LARGEUR, HAUTEUR);
+
+        var timer = new AnimationTimer() {
+            private long dernierTemps = System.nanoTime();
+
+            @Override
+            public void handle(long temps) {
+                double deltaTemps = (temps - dernierTemps) * 1e-9;
+
+                simulation.update(deltaTemps);
+                simulation.draw(contexte);
+
+                dernierTemps = temps;
+            }
+        };
+        timer.start();
+
+        stage.getIcons().add(new Image("logo.png"));
         stage.setTitle("GalakSIM");
         stage.setScene(scene);
         stage.show();
--- src/main/java/ca/qc/bdeb/sim/galak_sim/graphics/Simulation.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/graphics/Simulation.java
@@ -1,4 +1,13 @@
 package ca.qc.bdeb.sim.galak_sim.graphics;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Simulation {
+    public void update(double deltaTemps) {
+
+    }
+    
+    public void draw(GraphicsContext contexte) {
+
+    }
 }
