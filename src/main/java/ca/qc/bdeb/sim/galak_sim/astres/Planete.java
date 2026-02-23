aa58a4c24b9d0615d421f50fcb5dc57a00cd9ec0
MATITO3 <matlanglois2007@gmail.com>
2026-02-23 14:04:40 -0500

    Astre

--- src/main/java/ca/qc/bdeb/sim/galak_sim/astres/Planete.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/astres/Planete.java
@@ -1,4 +1,16 @@
 package ca.qc.bdeb.sim.galak_sim.astres;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class Planete {
+    public Planete(double x, double y, double largeur, double hauteur) {
+    }
+
+    public void update(double deltaTemps) {
+
+    }
+
+    public void draw(GraphicsContext contexte) {
+
+    }
 }
--- src/main/java/ca/qc/bdeb/sim/galak_sim/graphics/Simulation.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/graphics/Simulation.java
@@ -1,8 +1,17 @@
 package ca.qc.bdeb.sim.galak_sim.graphics;
 
+import ca.qc.bdeb.sim.galak_sim.astres.Planete;
 import javafx.scene.canvas.GraphicsContext;
 
+import java.util.ArrayList;
+
 public class Simulation {
+    private ArrayList<Planete> planetes = new ArrayList<>();
+
+    public Simulation() {
+        planetes.add(new Planete(0, 0, 20, 20));
+    }
+
     public void update(double deltaTemps) {
 
     }
