3c981adec08490e9b21e76483653c4ac404701a6
MatisLanglois <matlanglois2007@gmail.com>
2026-04-16 21:16:01 -0400

    Style et possibilité d'aller rapidement

--- src/main/java/ca/qc/bdeb/sim/galak_sim/MainJavaFX.java
+++ src/main/java/ca/qc/bdeb/sim/galak_sim/MainJavaFX.java
@@ -180,6 +180,7 @@ public class MainJavaFX extends Application {
         Text texteNom = new Text("Nom");
         texteNom.setFill(Color.WHITE);
         TextField saisiNom = new TextField();
+        saisiNom.setTextFormatter(formateurAlphabetique());
 
         Text texteVitesseX = new Text("Vitesse en x");
         texteVitesseX.setFill(Color.WHITE);
@@ -479,6 +480,15 @@ public class MainJavaFX extends Application {
         return new TextFormatter<>(change ->
                 change.getControlNewText().matches(regex) ? change : null
         );
+    }
+
+    public static TextFormatter<String> formateurAlphabetique() {
+        return new TextFormatter<>(change -> {
+            if (change.getControlNewText().length() <= 20) {
+                return change;
+            }
+            return null;
+        });
     }
 
     private void ajouterPlanete(MouseEvent e, Canvas canvas, TextField saisiVitesseX, TextField saisiVitesseY, TextField saisiMasse, TextField saisiNom, VBox listePlanete) {
